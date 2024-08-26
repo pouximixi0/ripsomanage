e0c6391407bfea7d7f52e7c5657392f7aee682e6
pouximixi0 <104493295+pouximixi0@users.noreply.github.com>
2024-08-26 13:39:40 +0200

    Update requierment.bat

--- requierment.bat
+++ requierment.bat
@@ -1,27 +1,34 @@
 @echo off
 color a
 cls
+pause
+@echo off
+color a
+cls
 
-echo       _                                                        
-echo      (_)                                                       
-echo  _ __ _ _ __  ___  ___  _ __ ___   __ _ _ __   __ _  __ _  ___ 
-echo | '__| | '_ \/ __|/ _ \| '_ ` _ \ / _` | '_ \ / _` |/ _` |/ _ \
-echo | |  | | |_) \__ \ (_) | | | | | | (_| | | | | (_| | (_| |  __/
-echo |_|  |_| .__/|___/\___/|_| |_| |_|\__,_|_| |_|\__,_|\__, |\___|
-echo        | |                                           __/ |     
-echo        |_|                                          |___/      
-echo Vous êtes sur le point d'installer les dépendances développeur de ripsomanage v1.0 Python
+echo   "         _							       "
+echo "      _ __(_)_ __  ___  ___  _ __ ___   __ _ _ __   __ _  __ _  ___      "
+echo "     | '__| | '_ \/ __|/ _ \| '_ ` _ \ / _` | '_ \ / _` |/ _` |/ _ \     "
+echo "     | |  | | |_) \__ \ (_) | | | | | | (_| | | | | (_| | (_| |  __/     "
+echo "     |_|  |_| .__/|___/\___/|_| |_| |_|\__,_|_| |_|\__,_|\__, |\___|     "
+echo   "          |_|                                          |___/	       "
+echo "Vous aite sur le point d'installer les dependances developpeur de ripsomanage v1.0 python"
 pause
 
 cls
 python -m pip install --upgrade pip
-
+cls
 pip install customtkinter
+cls
 pip install CTkMessagebox
+cls
 pip install psutil
+cls
 pip install GPUtil
+cls
 pip install py-cpuinfo
+cls
 pip install Nuitka
-
-echo Tous les paquets ont été installés et mis à jour.
+cls
+echo "Tout a ete installe et mis a jour"
 pause
