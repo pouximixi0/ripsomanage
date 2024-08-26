4aed3228f5549e636d78005a58846e21b6cab74d
pouximixi0 <104493295+pouximixi0@users.noreply.github.com>
2024-08-26 13:10:41 +0200

    Update requierment.bat

--- requierment.bat
+++ requierment.bat
@@ -1,10 +1,6 @@
 @echo off
-
-:: Mettre à jour pip
+color a
 python -m pip install --upgrade pip
-
-:: Installer les dépendances avec mise à jour
 pip install --upgrade -r requirements.txt
-
 echo tous a était instaler et mis a jour
 pause
