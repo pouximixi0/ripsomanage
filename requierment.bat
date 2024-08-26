4bf82c39f4b7f1cb60ce4b30e34edd19fed1a893
pouximixi0 <104493295+pouximixi0@users.noreply.github.com>
2024-08-26 13:28:02 +0200

    Update requierment.bat

--- requierment.bat
+++ requierment.bat
@@ -3,10 +3,12 @@ color a
 cls
 
 echo       _                                                        
-echo  _ __(_)_ __  ___  ___  _ __ ___   __ _ _ __   __ _  __ _  ___ 
+echo      (_)                                                       
+echo  _ __ _ _ __  ___  ___  _ __ ___   __ _ _ __   __ _  __ _  ___ 
 echo | '__| | '_ \/ __|/ _ \| '_ ` _ \ / _` | '_ \ / _` |/ _` |/ _ \
 echo | |  | | |_) \__ \ (_) | | | | | | (_| | | | | (_| | (_| |  __/
 echo |_|  |_| .__/|___/\___/|_| |_| |_|\__,_|_| |_|\__,_|\__, |\___|
+echo        | |                                           __/ |     
 echo        |_|                                          |___/      
 echo Vous êtes sur le point d'installer les dépendances développeur de ripsomanage v1.0 Python
 pause
